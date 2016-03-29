0efb4741592691c8458d65b292bf78951d52f12e
Debarshi Dutta <debarshi.dutta@research.iiit.ac.in>
2016-03-29 13:00:48 +0530

    prepared an initial main entry point for mcb.
    	new file:   mcb/Makefile
    	new file:   mcb/mcb_cycle.cpp

---
+++ mcb/mcb_cycle.cpp
@@ -0,0 +1,82 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <utility>
+#include <set>
+#include <stack>
+#include <omp.h>
+#include <string>
+#include <algorithm>
+#include <atomic>
+#include <list>
+#include <unordered_set>
+
+#include "FileReader.h"
+#include "Files.h"
+#include "utils.h"
+#include "Host_Timer.h"
+#include "CsrGraph.h"
+
+debugger dbg;
+HostTimer globalTimer;
+
+std::string InputFileName;
+std::string OutputFileDirectory;
+
+double totalTime = 0;
+
+int main(int argc,char* argv[])
+{
+	if(argc < 5)
+	{
+		printf("Ist Argument should indicate the InputFile\n");
+		printf("2nd Argument should indicate the outputdirectory\n");
+		printf("3th argument should indicate the number of threads.(Optional) (1 default)\n");
+		exit(1);
+	}
+
+	int num_threads = 1;
+
+	if(argc == 4)
+		num_threads = atoi(argv[3]);
+
+    	omp_set_num_threads(num_threads);
+
+    	//Open the FileReader class
+	std::string InputFilePath = InputFileName;
+
+	//Read the Inputfile.
+	FileReader Reader(InputFilePath.c_str());
+
+	int v1,v2,Initial_Vertices,weight;;
+
+	int nodes,edges;
+
+	//firt line of the input file contains the number of nodes and edges
+	Reader.get_nodes_edges(nodes,edges); 
+
+	csr_graph *graph=new csr_graph();
+
+	graph->Nodes = nodes;
+
+	/*
+	 * ====================================================================================
+	 * Fill Edges.
+	 * ====================================================================================
+	 */
+	for(int i=0;i<edges;i++)
+	{
+		Reader.read_edge(v1,v2,weight);
+		graph->insert(v1,v2,weight,false);
+	}
+
+
+	Reader.fileClose();
+
+	debug("Input File Reading Complete...\n");
+
+	return 0;
+}
