02c00b69cbc45b6a787b185ab469037af0c34482
Debarshi Dutta <debarshi.dutta@research.iiit.ac.in>
2016-04-07 18:35:46 +0530

    added the new test files for application.
    	modified:   mcb/Test.cpp

--- mcb/Test.cpp
+++ mcb/Test.cpp
@@ -20,6 +20,8 @@
 #include "Host_Timer.h"
 #include "CsrGraph.h"
 #include "CsrTree.h"
+#include "bit_vector.h"
+#include "work_per_thread.h"
 
 debugger dbg;
 HostTimer globalTimer;
@@ -79,71 +81,126 @@ int main(int argc,char* argv[])
 
 	Reader.fileClose();
 
+	std::vector<std::vector<unsigned> > *chains = new std::vector<std::vector<unsigned> >();
+
 	debug("Input File Reading Complete...\n");
 
 	int source_vertex;
 
-	std::vector<std::vector<unsigned> > *chains = new std::vector<std::vector<unsigned> >();
 	std::vector<unsigned> *remove_edge_list = graph->mark_degree_two_chains(&chains,source_vertex);
+	//initial_spanning_tree.populate_tree_edges(true,NULL,souce_vertex);
 
-	csr_tree initial_spanning_tree(graph);
-	std::vector<unsigned> *ear_decomposition = new std::vector<unsigned>(graph->Nodes + 1);
+	std::vector<std::vector<unsigned> > *edges_new_list = new std::vector<std::vector<unsigned> >();
 
-	//initial_spanning_tree.populate_tree_edges(true,ear_decomposition,source_vertex);
+	int nodes_removed = 0;
 
-	debug("Generating Initial Spanning Tree and Ear Decomposition");
+	for(int i=0;i<chains->size();i++)
+	{
+		unsigned row,col;
+		unsigned total_weight = graph->sum_edge_weights(chains->at(i),row,col);
 
-	debug("Number of Ears = ",ear_decomposition->at(graph->Nodes));
+		nodes_removed += chains->at(i).size() - 1;
 
-	debug("Tree - Edges");
+		std::vector<unsigned> new_edge = std::vector<unsigned>();
+		new_edge.push_back(row);
+		new_edge.push_back(col);
+		new_edge.push_back(total_weight);
 
-	unsigned row;
-	unsigned col;
+		edges_new_list->push_back(new_edge);
+		//debug(row+1,col+1,total_weight);
+	}
 
-	// for(int i=0;i<initial_spanning_tree.tree_edges->size();i++)
-	// {
-	// 	initial_spanning_tree.get_edge_endpoints(row,col,weight,
-	// 						 initial_spanning_tree.tree_edges->at(i));
-	// 	debug (row + 1,'-',col + 1,weight);
-	// }
+	debug ("Number of nodes removed = ",nodes_removed);
 
-	// debug("Non-Tree Edges");
+	csr_multi_graph *reduced_graph = csr_multi_graph::get_modified_graph(graph,
+									     remove_edge_list,
+									     edges_new_list,
+									     nodes_removed);
 
-	// for(int i=0;i<initial_spanning_tree.non_tree_edges->size();i++)
-	// {
-	// 	initial_spanning_tree.get_edge_endpoints(row,col,weight,
-	// 						 initial_spanning_tree.non_tree_edges->at(i));
-	// 	debug (row + 1,'-',col + 1,weight);
-	// }
+	//Node Validity
+	assert(reduced_graph->Nodes + nodes_removed == graph->Nodes);
 
-	// debug("Ear Decomposition");
+	reduced_graph->print_graph();
 
-	// for(int i=0;i<ear_decomposition->size() - 1;i++)
-	// 	debug(i+1,ear_decomposition->at(i));
+	csr_tree *initial_spanning_tree = new csr_tree(reduced_graph);
+	initial_spanning_tree->populate_tree_edges(true,source_vertex);
 
-	// debug("Number of degree 2 cycles =",chains->size());
+	int num_non_tree_edges = initial_spanning_tree->non_tree_edges->size();
 
-	// for(int i=0;i<chains->size();i++)
-	// {
-	// 	for(int j=0;j<chains->at(i).size();j++)
-	// 	{
-	// 		//printf("%u ",chains->at(i)[j]);
-	// 		unsigned offset = chains->at(i)[j];
-	// 		graph->get_edge_endpoints(row,col,weight,offset);
-	// 		printf("%u %u\n",row+1,col+1);
-	// 	}
-	// 	debug("");
-	// }
+	//Spanning Tree Validity
+	assert(num_non_tree_edges == reduced_graph->rows->size()/2 - reduced_graph->Nodes + 1);
 
-	// debug ("Removed Edges");
+	initial_spanning_tree->print_tree_edges();
+	initial_spanning_tree->print_non_tree_edges();
 
-	// for(int i=0;i<remove_edge_list->size();i++)
-	// {
-	// 	unsigned offset = remove_edge_list->at(i);
-	// 	graph->get_edge_endpoints(row,col,weight,offset);
 
-	// 	printf("%u - %u : %u\n",row+1,col+1,weight);
-	// }
+	worker_thread **multi_work = new worker_thread*[num_threads];
+	for(int i=0;i<num_threads;i++)
+		multi_work[i] = new worker_thread(reduced_graph);
 
-	 return 0;
+	//produce shortest path trees across all the nodes.
+	#pragma omp parallel for 
+	for(int i = 0; i < reduced_graph->Nodes; i++)
+	{
+		int threadId = omp_get_thread_num();
+		multi_work[threadId]->produce_sp_tree_and_cycles(i,reduced_graph);
+	}
+
+	std::vector<cycle*> list_cycle;
+
+	//block
+	{
+		int space[num_threads] = {0};
+		for(int i=0;i<num_threads;i++)
+			space[i] = multi_work[i]->list_cycles.size();
+
+		int prev = 0;
+		for(int i=0;i<num_threads;i++)
+		{
+			int temp = space[i];
+			space[i] = prev;
+			prev += temp;
+		}
+
+		//total number of cycles;
+		list_cycle.resize(prev);
+
+		#pragma omp parallel for
+		for(int i=0;i<num_threads;i++)
+		{
+			int threadId = omp_get_thread_num();
+			for(int j=0;j<multi_work[i]->list_cycles.size();j++)
+				list_cycle[space[i] + j] = multi_work[i]->list_cycles[j];
+
+			multi_work[i]->empty_cycles();
+
+		}
+
+	}
+
+	std::sort(list_cycle.begin(),list_cycle.end(),cycle::compare());
+
+	printf("List Cycles\n");
+	for(int i=0;i<list_cycle.size();i++)
+	{
+		printf("%u-(%u - %u) : %d\n",list_cycle[i]->get_root() + 1,
+					reduced_graph->rows->at(list_cycle[i]->non_tree_edge_index) + 1,
+					reduced_graph->columns->at(list_cycle[i]->non_tree_edge_index) + 1,
+					list_cycle[i]->total_length);
+	}
+
+	//At this stage we have the shortest path trees and the cycles sorted in increasing order of length.
+
+
+	//generate the bit vectors
+	bit_vector **support_vectors = new bit_vector*[num_non_tree_edges];
+	for(int i=0;i<num_non_tree_edges;i++)
+	{
+		*support_vectors = new bit_vector(num_non_tree_edges);
+		(*support_vectors)->set_bit(i,true);
+	}
+
+
+
+	return 0;
 }
