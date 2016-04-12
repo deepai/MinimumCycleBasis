87691e9d996cc2d425bbde61c532d6ca624bcbdd
Debarshi Dutta <debarshi.dutta@research.iiit.ac.in>
2016-04-12 10:19:27 +0530

    added precompute method to worker_thread.
    	modified:   include/work_per_thread.h

--- include/work_per_thread.h
+++ include/work_per_thread.h
@@ -5,6 +5,10 @@
 #include "CsrTree.h"
 #include "cycles.h"
 #include "Dijkstra.h"
+#include "bit_vector.h"
+
+#include <unordered_map>
+
 
 struct worker_thread
 {
@@ -47,11 +51,58 @@ struct worker_thread
 		}
 
 		sp_tree->remove_non_tree_edges();
+		sp_tree->node_pre_compute = new std::vector<unsigned>(graph->Nodes);
 	}
 
 	void empty_cycles()
 	{
 		list_cycles.clear();
+	}
+
+	void precompute_supportVec(std::unordered_map<unsigned,unsigned> &non_tree_edge_map,bit_vector &vector)
+	{
+		bool flag = false;
+
+		for(int i=0;i<shortest_path_trees.size();i++)
+		{
+			csr_tree *current_tree = shortest_path_trees.at(i);
+			
+			if(current_tree->root == 4)
+				flag = true;
+
+			std::vector<unsigned> *tree_edges = current_tree->tree_edges;
+			std::vector<unsigned> *precompute_nodes = current_tree->node_pre_compute;
+			csr_multi_graph *graph = current_tree->parent_graph;
+
+			precompute_nodes->at(current_tree->root) = 0;
+
+			unsigned edge_offset,reverse_edge,row,column,position,bit;
+
+			for(int i=0;i<tree_edges->size();i++)
+			{
+				edge_offset = tree_edges->at(i);
+				reverse_edge = graph->reverse_edge->at(edge_offset);
+				row = graph->rows->at(edge_offset);
+				column = graph->columns->at(edge_offset);
+
+				//non_tree_edge
+				if(non_tree_edge_map.find(edge_offset) != non_tree_edge_map.end())
+				{
+					bit = vector.get_bit(non_tree_edge_map.at(edge_offset));
+					precompute_nodes->at(column) = (precompute_nodes->at(row) + bit)%2;
+				}
+				else if(non_tree_edge_map.find(reverse_edge) != non_tree_edge_map.end())
+				{
+					bit = vector.get_bit(non_tree_edge_map.at(reverse_edge));
+					precompute_nodes->at(column) = (precompute_nodes->at(row) + bit)%2;
+				}
+				else //tree edge
+					precompute_nodes->at(column) = precompute_nodes->at(row);
+			}
+
+			flag = false;
+
+		}
 	}
 };
 
