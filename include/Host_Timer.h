ea6242ee9d6db2bdd9094d59dd58cde141ff23a7
Debarshi Dutta <debarshi.dutta@research.iiit.ac.in>
2016-04-13 10:35:01 +0530

    added the timing information to mcb_cycle.h and also added a new function to Host_Timer.h
    	modified:   ../include/Host_Timer.h
    	modified:   mcb_cycle.cpp

--- include/Host_Timer.h
+++ include/Host_Timer.h
@@ -11,6 +11,12 @@ public:
 
 	double start_timer();
 	double stop_timer();
+	double get_event_time()
+	{
+		double initial_time = elapsedTime;
+		elapsedTime = stop_timer();
+		return (elapsedTime - initial_time);
+	}
 
 };
 
